e1e73f2b44abb76b3e730543a8a89b35141fa74f
Puru Vijay <devpuruvj@gmail.com>
2021-07-18 18:40:03 +0530

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import DraggableCore from '../../package/DraggableCore.svelte';
+</script>
+
+<DraggableCore on:drag={(e) => console.log(e)} />
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
+
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+
+    package: {
+      emitTypes: true,
+    },
+  },
+};
+
+export default config;
