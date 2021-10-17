ab9d377e3126094264b7851de191719635bcf2dd
Tas Hasting <tas.hasting@gmail.com>
2021-10-16 20:11:05 -0500

    Add CancelDraggable and tests

--- tests/Draggable.spec.ts
+++ tests/Draggable.spec.ts
@@ -1,4 +1,3 @@
-// NOTE: jest-dom adds handy assertions to Jest and it is recommended, but not required.
 import '@testing-library/jest-dom';
 
 import { render } from '@testing-library/svelte';
---
+++ tests/components/CancelDraggable.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { draggable } from '../../src';
+</script>
+
+<div use:draggable={{ cancel: '.cancel' }}>
+  This will drag!
+  <div class="cancel">You shall not drag!!</div>
+</div>
