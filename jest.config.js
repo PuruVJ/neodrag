d8441128d0e5a900e6eca493f1335cd9436a9cd9
Tas <32209335+dwagio@users.noreply.github.com>
2021-10-17 10:42:13 -0500

    Add DOM testing (#14)
    * Add test-library & ts-jest infrastructure
    
    * Add @babel/preset-env
    
    * Add tests for basic draggable element
    
    * Use spec
    
    * Fix bounded tests
    
    * Top left bounds test change
    
    * Use universal draggable component
    
    * Add CancelDraggable and tests
    
    * Add HandleDraggable and tests
    
    * Add grid testing
    
    * Add gpuAcceleration tests
    
    * Disabled behavior
    
    * Delete package-lock.json
    
    * Use pnpm for lock
    
    * Use tsm
    
    Co-authored-by: Puru Vijay <47742487+PuruVJ@users.noreply.github.com>

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+const config = {
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.svelte$': [
+      'svelte-jester',
+      {
+        preprocess: true,
+      },
+    ],
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleFileExtensions: ['js', 'ts', 'svelte'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  testEnvironment: 'jsdom',
+  transformIgnorePatterns: ['/node_modules/(?!(.+\\.svelte$))', '\\.pnp\\.[^\\/]+$'],
+};
+
+export default config;
---
+++ tests/components/Draggable.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { DragBounds } from '../../dist';
+
+  import { draggable } from '../../src';
+
+  export let body = 'Drag me!';
+  export let axis: 'both' | 'x' | 'y' | 'none' = 'both';
+  export let bounds: DragBounds | undefined = undefined;
+  export let gpuAcceleration = true;
+  export let disabled = false;
+  export let grid: [number, number] | undefined = undefined;
+</script>
+
+<div id="drag-target" use:draggable={{ axis, bounds, gpuAcceleration, disabled, grid }}>{body}</div>
