d41a6a0e6d14d152be81b37f37d5a97cb4a26c1f
Puru Vijay <awesomepuruvj@gmail.com>
2022-01-27 21:49:41 +0530

    Add mdsvex

---
+++ docs/mdsvex.config.js
@@ -0,0 +1,13 @@
+// @ts-check
+const config = {
+	extensions: ['.svelte.md', '.md', '.svx'],
+
+	smartypants: {
+		dashes: 'oldschool'
+	},
+
+	remarkPlugins: [],
+	rehypePlugins: []
+};
+
+export default config;
--- docs/svelte.config.js
+++ docs/svelte.config.js
@@ -1,11 +1,15 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [preprocess(), mdsvex(mdsvexConfig)],
 
 	kit: {
 		adapter: adapter(),
