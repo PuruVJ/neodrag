f35e02f405873c22b0a7b1afcf7161976695ba53
Puru Vijay <devpuruvj@gmail.com>
2022-09-27 17:24:37 +0530

    Push after updating

--- packages/svelte/demo/svelte.config.js
+++ packages/svelte/demo/svelte.config.js
@@ -1,20 +1,12 @@
-//@ts-check
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess(),
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
 
-  kit: {
-    adapter: adapter(),
-
-    package: {
-      emitTypes: true,
-    },
-  },
+	kit: {},
 };
 
 export default config;
