67f3ccd2560f787a9fdae7885665dc48f57b0f45
Puru Vijay <devpuruvj@gmail.com>
2022-09-28 18:42:46 +0530

    Basic example

---
+++ docs/src/components/examples/Basic.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { draggable } from '@neodrag/svelte';
+</script>
+
+<div use:draggable>Drag me</div>
+
+<style>
+	div {
+		width: 100px;
+		height: 100px;
+		background: var(--cyan-1);
+	}
+</style>
