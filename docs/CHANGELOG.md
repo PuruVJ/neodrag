1754852b9f5a53689dec2e1adbcc57d8602fc0c1
Puru Vijay <devpuruvj@gmail.com>
2022-11-06 19:13:54 +0530

    Minor changes

--- docs/CHANGELOG.md
+++
@@ -1,8 +0,0 @@
-# @example/basics
-
-## 0.0.2
-
-### Patch Changes
-
-- Updated dependencies [[`f2d1130`](https://github.com/PuruVJ/neodrag/commit/f2d113052954b055fda7516919e4113bbde849d4)]:
-  - @neodrag/svelte@2.0.0
---
+++ docs/astro.config.ts
@@ -0,0 +1,28 @@
+// @ts-check
+import mdx from '@astrojs/mdx';
+import prefetch from '@astrojs/prefetch';
+import svelte from '@astrojs/svelte';
+import compress from 'astro-compress';
+
+import { defineConfig } from 'astro/config';
+
+import { fastDimension } from 'svelte-fast-dimension';
+import autoPreprocess from 'svelte-preprocess';
+import sequential from 'svelte-sequential-preprocessor';
+
+import remarkCustomContainer from 'remark-custom-container';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		svelte({ preprocess: sequential([autoPreprocess(), fastDimension()]) }),
+		mdx(),
+		prefetch(),
+		compress(),
+	],
+
+	markdown: {
+		// @ts-ignore
+		remarkPlugins: [remarkCustomContainer],
+	},
+});
