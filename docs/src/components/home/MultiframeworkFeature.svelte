3a862412257e3fb1f149bd7cbe9a983ab1977d29
Puru Vijay <awesomepuruvj@gmail.com>
2022-12-17 12:51:00 +0530

    Push

--- docs/src/components/home/MultiframeworkFeature.svelte
+++ docs/src/components/home/MultiframeworkFeature.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	// TODO: MAKE IT FANCYYCYYYYYYYYYYYY 😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡
 	import { FRAMEWORKS } from 'src/helpers/constants';
-	import { onMount } from 'svelte';
 	import SpinnerItem from './SpinnerItem.svelte';
 
 	// let frameworks = ['vanilla', 'svelte', 'react', 'vue', 'solid'];
@@ -9,28 +8,15 @@
 		(name) => FRAMEWORKS.find((f) => f.name === name)!
 	)!;
 
-	let spinnerHeight = 0;
-	let theta = (2 * Math.PI) / 3;
-	$: radius =
-		spinnerHeight === 0 ? 0 : spinnerHeight / (2 * Math.sin(theta / 2));
-
-	let mouseY: number | null = null;
+	const baseAngle = (2 * Math.PI) / frameworks.length;
 </script>
 
 <section class="animation-frameworks">
-	<span class="prefix"> pnpm add @neodrag/ </span>
-	<ul
-		class="spinner"
-		bind:clientHeight={spinnerHeight}
-		on:mousemove={(e) => (mouseY = e.clientY)}
-		on:mouseleave={() => (mouseY = null)}
-	>
-		{#each frameworks as { name }, idx}
-			<li style:color="var(--app-color-brand-{name})" class="framework">
-				<SpinnerItem {spinnerHeight} frameworkName={name} {mouseY} />
-			</li>
-		{/each}
-	</ul>
+	{#each frameworks as { name }}
+		<button>
+			<img src="/framework-logos/{name}.svg" alt="{name} logo" />
+		</button>
+	{/each}
 </section>
 
 <style lang="scss">
