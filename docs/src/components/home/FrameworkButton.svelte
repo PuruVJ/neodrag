f071e4227b13c0722bf921332e740c273d253381
Puru Vijay <devpuruvj@gmail.com>
2022-12-18 16:55:39 +0530

    Push the polygon

---
+++ docs/src/components/home/FrameworkButton.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+	import type { Framework } from 'src/helpers/constants';
+	import { draggable } from '@neodrag/svelte';
+	import { portal } from '$actions/portal';
+	import { fade } from 'svelte/transition';
+
+	export let logoEl: HTMLImageElement;
+	export let framework: Framework;
+
+	let buttonEl: HTMLButtonElement;
+
+	let lineProperties = {
+		thickness: 2,
+		left: 0,
+		top: 0,
+		width: 0,
+		angle: 0,
+	};
+
+	function getOffset(el: HTMLElement) {
+		const rect = el.getBoundingClientRect();
+
+		return {
+			left: rect.left + window.scrollX,
+			top: rect.top + window.scrollY,
+			width: rect.width || el.offsetWidth,
+			height: rect.height || el.offsetHeight,
+		};
+	}
+
+	const THICKNESS = 2;
+
+	function doThing(node: HTMLElement, rootEl: HTMLElement) {
+		if (!rootEl) return;
+
+		const rootRect = getOffset(rootEl);
+		const nodeRect = getOffset(node);
+
+		const x1 = rootRect.left + rootRect.width / 2;
+		const y1 = rootRect.top + rootRect.height / 2;
+		// top right
+		const x2 = nodeRect.left + nodeRect.width / 2;
+		const y2 = nodeRect.top + nodeRect.height / 2;
+		// distance
+		const length = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+		const cx = (x1 + x2) / 2 - length / 2;
+		const cy = (y1 + y2) / 2 - THICKNESS / 2;
+		// angle
+		const angle = Math.atan2(y1 - y2, x1 - x2) * (180 / Math.PI);
+
+		lineProperties = {
+			thickness: THICKNESS,
+			left: cx,
+			top: cy,
+			width: length,
+			angle,
+		};
+	}
+
+	function connect(node: HTMLElement, rootEl: HTMLElement) {
+		// TODO: Investigate replacing with resizeobserver
+		window.addEventListener('resize', () => doThing(node, rootEl));
+
+		return {
+			update(newRootEl: HTMLElement) {
+				rootEl = newRootEl;
+				doThing(node, rootEl);
+			},
+		};
+	}
+</script>
+
+<button
+	bind:this={buttonEl}
+	use:draggable={{ onDrag: () => doThing(buttonEl, logoEl) }}
+	use:connect={logoEl}
+>
+	<slot />
+</button>
+
+{#if logoEl}
+	<div
+		in:fade={{ delay: 1000 }}
+		class="line"
+		data-logo-framework-connector
+		data-framework={framework}
+		use:portal={'body'}
+		style:--top="{lineProperties.top}px"
+		style:--left="{lineProperties.left}px"
+		style:--length="{lineProperties.width}px"
+		style:--thickness="{lineProperties.thickness}px"
+		style:--angle="{lineProperties.angle}deg"
+	/>
+{/if}
+
+<style lang="scss">
+	button {
+		background-color: transparent;
+	}
+
+	.line {
+		position: absolute;
+		top: var(--top);
+		left: var(--left);
+
+		padding: 0px;
+		margin: 0px;
+
+		background-color: hsla(var(--app-color-dark-hsl), 0.5);
+		line-height: 1px;
+
+		height: var(--thickness);
+		width: var(--length);
+
+		transform: rotate(var(--angle));
+	}
+</style>
--- docs/src/components/home/MultiframeworkFeature.svelte
+++ docs/src/components/home/MultiframeworkFeature.svelte
@@ -1,26 +1,93 @@
 <script lang="ts">
 	// TODO: MAKE IT FANCYYCYYYYYYYYYYYY 😡😡😡😡😡😡😡😡😡😡😡😡😡😡😡
 	import { FRAMEWORKS } from 'src/helpers/constants';
-	import SpinnerItem from './SpinnerItem.svelte';
+
+	// @ts-ignore
+	import LogosSvelteIcon from '~icons/logos/svelte-icon';
+	// @ts-ignore
+	import LogosReactIcon from '~icons/logos/react';
+	// @ts-ignore
+	import LogosVueIcon from '~icons/logos/vue';
+	// @ts-ignore
+	import LogosSolidIcon from '~icons/logos/solidjs-icon';
+	// @ts-ignore
+	import LogosVanillaIcon from '~icons/logos/javascript';
+
+	import FrameworkButton from './FrameworkButton.svelte';
+
+	let logoEl: HTMLImageElement;
 
 	// let frameworks = ['vanilla', 'svelte', 'react', 'vue', 'solid'];
-	let frameworks = ['react', 'vue', 'svelte', 'solid', 'vanilla'].map(
-		(name) => FRAMEWORKS.find((f) => f.name === name)!
-	)!;
-
-	const baseAngle = (2 * Math.PI) / frameworks.length;
+	const frameworks = ['solid', 'vanilla', 'react', 'vue', 'svelte']
+		.map((name) => FRAMEWORKS.find((f) => f.name === name)!)!
+		.map(({ name }) => ({
+			name,
+			icon: {
+				svelte: LogosSvelteIcon,
+				react: LogosReactIcon,
+				vue: LogosVueIcon,
+				solid: LogosSolidIcon,
+				vanilla: LogosVanillaIcon,
+			}[name],
+		}));
 </script>
 
+<img bind:this={logoEl} src="/logo.svg" class="logo" alt="Neodrag logo" />
+
 <section class="animation-frameworks">
-	{#each frameworks as { name }}
-		<button>
-			<img src="/framework-logos/{name}.svg" alt="{name} logo" />
-		</button>
+	{#each frameworks as { name, icon: Icon }}
+		<div>
+			<FrameworkButton framework={name} {logoEl}>
+				<Icon />
+			</FrameworkButton>
+		</div>
 	{/each}
 </section>
 
 <style lang="scss">
+	@mixin on-circle($item-count, $circle-size, $item-size) {
+		position: relative;
+		width: $circle-size;
+		height: $circle-size;
+		padding: 0;
+		border-radius: 50%;
+		list-style: none;
+
+		> * {
+			display: block;
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			width: $item-size;
+			height: $item-size;
+			margin: calc(-1 * ($item-size / 2));
+
+			$angle: calc(360 / $item-count);
+			$rot: -18;
+
+			@for $i from 1 through $item-count {
+				&:nth-of-type(#{$i}) {
+					transform: rotate(calc($rot * 1deg))
+						translate(calc($circle-size / 2))
+						rotate(calc($rot * -1deg));
+				}
+
+				$rot: $rot + $angle;
+			}
+		}
+	}
+
+	.logo {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		width: 10rem;
+	}
+
 	.animation-frameworks {
+		@include on-circle($item-count: 5, $circle-size: 40rem, $item-size: 4rem);
+
 		display: flex;
 		align-items: flex-start;
 
@@ -29,23 +96,5 @@
 		font-size: 3rem;
 		font-weight: 600;
 		font-family: 'Jetbrains Mono', monospace;
-	}
-
-	.spinner {
-		display: flex;
-		flex-direction: column;
-		gap: 0rem;
-
-		list-style-type: none;
-
-		padding: 0;
-
-		font-size: inherit;
-
-		width: 40%;
-	}
-
-	.framework {
-		transform-origin: left;
 	}
 </style>
