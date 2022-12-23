712a2328c002b8b8a496924b50651638385a919f
Puru Vijay <devpuruvj@gmail.com>
2022-12-23 14:22:55 +0530

    Update svelte version to export events in action

--- packages/svelte/src/index.ts
+++ packages/svelte/src/index.ts
@@ -1,1 +1,20 @@
-export * from '@neodrag/core';
+import { DragEventData, draggable as coreDraggable, DragOptions } from '@neodrag/core';
+import type { Action } from 'svelte/action';
+
+export const draggable = coreDraggable as Action<
+	HTMLElement,
+	DragOptions,
+	{
+		'on:neodrag:start': (e: CustomEvent<DragEventData>) => void;
+		'on:neodrag': (e: CustomEvent<DragEventData>) => void;
+		'on:neodrag:end': (e: CustomEvent<DragEventData>) => void;
+	}
+>;
+
+export type {
+	DragAxis,
+	DragBounds,
+	DragBoundsCoords,
+	DragEventData,
+	DragOptions,
+} from '@neodrag/core';
