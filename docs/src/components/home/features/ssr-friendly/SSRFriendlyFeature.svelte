d0dc42ced98d94c76548ec9fc9ff2c1839a1a580
Puru Vijay <devpuruvj@gmail.com>
2022-12-24 21:52:55 +0530

    Push SSR friendly feature

---
+++ docs/src/components/home/features/ssr-friendly/SSRFriendlyFeature.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { theme } from '$stores/user-preferences.store';
+</script>
+
+<div class="intro">
+	<h2>SSR-friendly</h2>
+	<p>
+		Neodrag is Server Side Renderable. Will play well with meta-frameworks like
+		Sveltekit, NextJS, Nuxt, Vitepress, SolidStart and more
+	</p>
+</div>
+
+<div class="frameworks-list">
+	<img
+		src="/home/metaframeworks-{$theme}.webp"
+		loading="lazy"
+		alt="Metaframeworks"
+	/>
+</div>
+
+<style lang="scss">
+	img {
+		width: clamp(10rem, 60%, 30rem);
+	}
+
+	.frameworks-list {
+		display: flex;
+		justify-content: center;
+	}
+</style>
--- docs/src/stores/user-preferences.store.ts
+++ docs/src/stores/user-preferences.store.ts
@@ -1,8 +1,4 @@
 import { writable } from 'svelte-local-storage-store';
 
-import type { Framework } from 'src/helpers/constants';
-
-export const selectedFramework = writable<Framework>('neodrag:selectedFramework', 'svelte');
-
 export type Theme = 'light' | 'dark';
 export const theme = writable<Theme>('neodrag:theme', 'light');
