13d1b3231942383e4b6b11da7413e4b2b060f03a
Houtan Rocky <mast9rmind@gmail.com>
2024-04-20 02:54:39 +0330

    Update UMD configuration for vanilla demo

--- packages/vanilla/demo-umd/index.js
+++
@@ -1,6 +0,0 @@
-const dragEl = document.querySelector('.box');
-if (!dragEl) {
-  throw new Error('Drag element not found');
-}
-const dragInstance = new NeoDragVanilla.Draggable(dragEl);
-console.log({ dragInstance });
