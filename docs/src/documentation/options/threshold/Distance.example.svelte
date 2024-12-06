3e7b7c45b3e637892376997a5e17cc09f025e7da
Puru Vijay <awesomepuruvj@gmail.com>
2024-12-06 22:50:39 +0530

    Add documentation

---
+++ docs/src/documentation/options/threshold/Distance.example.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import OptionsDemoBase from '$components/options/OptionsDemoBase.svelte';
+</script>
+
+<OptionsDemoBase
+	options={{
+		threshold: {
+			distance: 100,
+		},
+	}}
+>
+	100px distance
+</OptionsDemoBase>
